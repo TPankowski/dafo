cc3027934db2809e074b581e618d1ebf6fe2c54b
TPankowski <52370435+TPankowski@users.noreply.github.com>
2019-09-22 21:13:48 +0200

    Add files via upload

---
+++ bib_db/BIB_DB-create.bat
@@ -0,0 +1,9 @@
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\BIB_DB-create.sql -o .\msg\BIB_DB-create.txt
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\1_Person.sql -o .\msg\1_Person.txt
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\2_Conference.sql -o .\msg\2_Conference.txt
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\3_Proceedings.sql -o .\msg\3_Proceedings.txt
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\4_Paper.sql -o .\msg\4_Paper.txt
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\5_Affiliation.sql -o .\msg\5_Affiliation.txt
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\6_AuthorPaper.sql -o .\msg\6_AuthorPaper.txt
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\7_PCMember.sql -o .\msg\7_PCMember.txt
+sqlcmd -S SATURN\SQL2017 -v dbName ="BIB_DB" -i sql\8_mv.sql -o .\msg\8_mv.txt
---
+++ bib_db/sql/BIB_DB-create.sql
@@ -0,0 +1,12 @@
+use Master
+go
+drop database $(dbName)
+go
+create database $(dbName)
+go
+use $(dbName)
+go
+set nocount on
+go
+
+
